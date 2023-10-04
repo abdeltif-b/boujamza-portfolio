f1e3f74603c72e08d75658ee3d59dabc1c4edd06
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-04 09:25:20 +0100

    add shadcn ui components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,11 @@
-export const validateString = (
-  value: unknown,
-  maxLength: number
-): value is string => {
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const validateString = (value: unknown, maxLength: number): value is string => {
   if (!value || typeof value !== "string" || value.length > maxLength) {
     return false;
   }
