ad0281ef2aaba62ed88fadd706b3affc4e827f53
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-05 00:58:32 +0100

    add theme switch support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import "./globals.css";
 import { CustomToaster } from "@/components/base/toaster";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import MenuBar from "@/components/menubar";
+import ThemeSwitch from "@/components/theme-switch";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +18,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <CustomToaster />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <MenuBar />
+
+          {children}
+          <Footer />
+          <ThemeSwitch />
+          <CustomToaster />
+        </ThemeProvider>
       </body>
     </html>
   );
