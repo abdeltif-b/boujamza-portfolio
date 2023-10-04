305bf205931845b70f6340909355f5997eeb6a5a
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-04 11:42:48 +0100

    add custom toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
+import { CustomToaster } from "@/components/base/toaster";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Toaster position="top-right" />
+        <CustomToaster />
       </body>
     </html>
   );
