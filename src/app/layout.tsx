16e967939fc3860bcfd6d346310d985102dec5c3
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-06 15:32:02 +0100

    use light as default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
           <MenuBar />
           {children}
           <Footer />
--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 import React from "react";
@@ -20,7 +19,7 @@ export default function ThemeSwitch() {
       className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
       onClick={toggleTheme}
     >
-      {theme === "light" ? <MoonIcon /> : <SunIcon />}
+      {theme === "dark" ? <SunIcon /> : <MoonIcon />}
     </button>
   );
 }
