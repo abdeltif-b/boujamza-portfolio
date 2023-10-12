28e9bb1ce2ef5d6b7488d8c19da2e6036fa39c0c
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-12 19:22:49 +0100

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,7 +11,7 @@ export default async function Footer() {
   return (
     <footer className="p-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; {currentYear} <b>Portfolio/&gt;</b>. All rights reserved.
+        &copy; {currentYear} <b>Boujamza/&gt;</b>. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with Next.js & React, TypeScript, Tailwind CSS,
