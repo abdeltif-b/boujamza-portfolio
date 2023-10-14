d5dc66b1ae35fce69f810c86ff09a194044c548f
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-14 01:15:37 +0100

    fix typescript error for an experimental hook

--- src/components/submit-btn.tsx
+++ src/components/submit-btn.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
-import { useFormStatus } from "react-dom";
+// @ts-expect-error
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
 export default function SubmitBtn() {
   const { pending } = useFormStatus();
